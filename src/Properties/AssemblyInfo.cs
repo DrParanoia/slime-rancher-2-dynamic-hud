cad92e6ae47123d03ca0d0f4cc5fdc750826badb
Arturs Ziborovs <arturs.ziborovs@gmail.com>
2026-04-23 19:10:01 +0300

    Bump version to 1.2.0 (matching SR2 game version)

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
 using DynamicHud;
 
-[assembly: MelonInfo(typeof(DynamicHudMod), "Dynamic HUD", "1.0.0", "DrParanoia")]
+[assembly: MelonInfo(typeof(DynamicHudMod), "Dynamic HUD", "1.2.0", "DrParanoia")]
 [assembly: MelonGame("MonomiPark", "SlimeRancher2")]
